ba0d01d03a1906ef0b589f2115b4cbcbb021164a
Tulsi Keswani <tulsikeswani7171@gmail.com>
2025-04-16 13:14:21 +0530

    Not found styling updated

--- Frontend/src/pages/NotFound/NotFound.jsx
+++ Frontend/src/pages/NotFound/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Notfound.css";
 function NotFound() {
     return (
-        <div className=".NotFoundContainer">
+        <div className="NotFoundContainer">
             <img src="/assets/videocall2_image.webp" alt="videcall image"/>
             <h2>hmm... Sorry Page Not Found</h2>
         </div>
