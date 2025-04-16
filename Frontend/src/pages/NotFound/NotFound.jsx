7edfd90d818e2118e7b279e939e74dec1bb3341d
Tulsi Keswani <tulsikeswani7171@gmail.com>
2025-04-16 13:10:54 +0530

    Not found styling updated

--- Frontend/src/pages/NotFound/NotFound.jsx
+++ Frontend/src/pages/NotFound/NotFound.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function NotFound() {
     return (
-        <div style={{display : "flex",justifyContent : "center",alignItms:"center",height:"100vh",width:"100vw"}}>
-            <img src="/assets/videocall2_image.webp" alt="" style={{height:"50vh",width:"50vw"}}/>
-
+        <div style={{display : "flex",justifyContent : "center",alignItms:"center",height:"100vh",width:"100vw",fle}}>
+            <img src="/assets/videocall2_image.webp" alt="videcall image" style={{height}}/>
+            <h2>hmm... Sorry Page Not Found</h2>
         </div>
      );
 }
