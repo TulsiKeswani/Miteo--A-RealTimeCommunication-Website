c13c11a5c6916ee951d9138879710f3ea5cffabf
Tulsi Keswani <tulsikeswani7171@gmail.com>
2025-04-16 13:00:58 +0530

    Not found image change

--- Frontend/src/pages/NotFound/NotFound.jsx
+++ Frontend/src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NotFound() {
     return (
         <div style={{display : "flex",justifyContent : "center",alignItms:"center",height:"100vh",width:"100vw"}}>
-            <img src="/assets/videocall_img.png" alt="" style={{height:"50vh",width:"50vw"}}/>
+            <img src="/assets/videocall2_img.png" alt="" style={{height:"50vh",width:"50vw"}}/>
 
         </div>
      );
