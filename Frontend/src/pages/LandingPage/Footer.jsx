f5450f359506bcf8cf6a5677c502718b5b951d77
Tulsi Keswani <tulsikeswani7171@gmail.com>
2025-04-16 13:38:07 +0530

    updated Footer

--- Frontend/src/pages/LandingPage/Footer.jsx
+++ Frontend/src/pages/LandingPage/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Footer.css";
-
+import Dialog from "./Dialog.jsx";
 export default function Footer() {
   return (
     <footer>
-        <p>© 2025 Miteo. All rights reserved. | <a href="/terms">Terms & Conditions</a> | <a href="/privacy-policy">Privacy Policy</a></p>
+        <p>© 2025 Miteo. All rights reserved. | <Dialog></Dialog>| <a href="/privacy-policy">Privacy Policy</a></p>
     </footer>
   );
 }
