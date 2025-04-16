d9489ad0da406b082f30ef1ae531cdcaa5b21820
Tulsi Keswani <tulsikeswani7171@gmail.com>
2025-04-16 20:41:16 +0530

    Error fix

--- Frontend/src/pages/LandingPage/Footer.jsx
+++ Frontend/src/pages/LandingPage/Footer.jsx
@@ -4,8 +4,7 @@ import {ScrollDialog,ScrollDialog2} from "./Dialog.jsx";
 export default function Footer() {
   return (
     <footer>
-import {ScrollDialog,} from "./Dialog.jsx";
-<p>© 2025 Miteo. All rights reserved. | <ScrollDialog></ScrollDialog>| <ScrollDialog2></ScrollDialog2> </p>
+      <p>© 2025 Miteo. All rights reserved. | <ScrollDialog></ScrollDialog>| <ScrollDialog2></ScrollDialog2> </p>
     </footer>
   );
 }
