b0d7d33b773fb65883de260dcb2744ac78ddfb07
Tulsi Keswani <tulsikeswani7171@gmail.com>
2025-04-16 13:40:51 +0530

    updated Footer

--- Frontend/src/pages/LandingPage/Dialog.jsx
+++ Frontend/src/pages/LandingPage/Dialog.jsx
@@ -5,7 +5,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-
+import Typography from "@mui/material/Typography";
 export default function Dialog() {
   const [open, setOpen] = React.useState(false);
   const [scroll, setScroll] = React.useState("paper");
